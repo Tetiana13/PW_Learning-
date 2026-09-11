dd4c2f4a3f92779e20041915d9287b2b6dfeba62
Tetiana Kupriianova <tanja.kasja@gmail.com>
2026-09-12 00:28:19 +0200

    Refactor authentication tests and add dotenv for environment variable management

--- fixtures/app.fixtures.ts
+++ fixtures/app.fixtures.ts
@@ -2,24 +2,48 @@ import { test as base, expect } from '@playwright/test';
 import { App } from '../pages/app';
 import { users } from '../data/auth';
 
+
 type Fixtures = {
   app: App;
   loggedInApp: App;
+};
+
+type LoginResponse = {
+  access_token: string;
 };
 
 export const test = base.extend<Fixtures>({
   app: async ({ page }, use) => {
     await use(new App(page));
   },
-  loggedInApp: async ({ app }, use) => {
-  await app.page.goto('/auth/login');
-  await app.loginPage.performLogin(
-    users.customer_1.login,
-    users.customer_1.password,
-  );
-  await expect(app.page).toHaveURL('/account');
-  await use(app);
-},
+
+  loggedInApp: async ({ app, request }, use) => {
+    const response = await request.post(
+      `${process.env.API_URL}/users/login`,
+      {
+        data: {
+          email: users.customer_1.login,
+          password: users.customer_1.password,
+        },
+     },
+    );
+
+    expect(response.ok()).toBeTruthy();
+    const jsonData = (await response.json()) as LoginResponse;
+    const token = jsonData.access_token;
+
+    await app.page.goto('/');
+
+    await app.page.evaluate((token) => {
+      localStorage.setItem('auth-token', token);
+    }, token);
+
+    await app.page.goto('/account');
+
+    await expect(app.page).toHaveURL('/account');
+
+    await use(app);
+  },
 });
 
 export { expect };
--- tests/auth.login.spec.ts
+++ tests/auth.login.spec.ts
@@ -1,8 +1,6 @@
 import { expect, test } from '../fixtures/app.fixtures';
 import { authFile } from '../utils/auth';
 
-test.skip(!!process.env.CI, 'Skip on CI due to Cloudflare verification');
-
 test.use({ storageState: authFile });
 
 test('Login with valid credentials', async ({ loggedInApp }) => {
